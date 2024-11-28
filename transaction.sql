79de3a51cf6f0e74609bd5ecf010bdab094d3177
RosLaffey <neptsun@qq.com>
2024-11-28 12:42:56 +0800

    Update transaction.sql

--- transaction.sql
+++ transaction.sql
@@ -1,39 +1,94 @@
--- 创建数据库
-USE transaction_project;
+/*
+ Navicat Premium Dump SQL
 
--- 区块信息表
-CREATE TABLE IF NOT EXISTS t_block_info (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    block_index VARCHAR(64) NOT NULL COMMENT '区块索引',
-    current_block_index VARCHAR(64) NOT NULL COMMENT '当前区块索引',
-    create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    update_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='区块信息表';
+ Source Server         : test
+ Source Server Type    : MySQL
+ Source Server Version : 80020 (8.0.20)
+ Source Host           : localhost:3306
+ Source Schema         : testprj
 
--- 链数据表
-CREATE TABLE IF NOT EXISTS t_chain_data (
-    id VARCHAR(64) PRIMARY KEY COMMENT '主键ID',
-    data TEXT NOT NULL COMMENT '交易数据',
-    create_time VARCHAR(32) NOT NULL COMMENT '创建时间',
-    hash VARCHAR(64) NOT NULL COMMENT '哈希值',
-    block_index VARCHAR(64) DEFAULT NULL COMMENT '区块索引'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='链数据表';
+ Target Server Type    : MySQL
+ Target Server Version : 80020 (8.0.20)
+ File Encoding         : 65001
 
--- 产品表
-CREATE TABLE IF NOT EXISTS t_product (
-    id VARCHAR(64) PRIMARY KEY COMMENT '主键ID',
-    name VARCHAR(255) NOT NULL COMMENT '产品名称',
-    create_time VARCHAR(32) NOT NULL COMMENT '创建时间'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='产品表';
+ Date: 27/11/2024 19:26:22
+*/
 
--- 编码表
-CREATE TABLE IF NOT EXISTS t_code (
-    id VARCHAR(64) PRIMARY KEY COMMENT '主键ID',
-    code VARCHAR(255) NOT NULL COMMENT '编码',
-    product_id VARCHAR(64) NOT NULL COMMENT '产品ID',
-    create_time VARCHAR(32) NOT NULL COMMENT '创建时间',
-    FOREIGN KEY (product_id) REFERENCES t_product(id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='编码表';
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
--- 初始化区块信息
-INSERT INTO t_block_info (block_index, current_block_index) VALUES ('0', '0'); 
+-- ----------------------------
+-- Table structure for t_block
+-- ----------------------------
+DROP TABLE IF EXISTS `t_block`;
+CREATE TABLE `t_block`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `blockIndex` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '区块链最大区块号',
+  `currentBlockIndex` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '当前项目更新到的区块链高度',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of t_block
+-- ----------------------------
+INSERT INTO `t_block` VALUES (1, '0', '0');
+
+-- ----------------------------
+-- Table structure for t_chain_data
+-- ----------------------------
+DROP TABLE IF EXISTS `t_chain_data`;
+CREATE TABLE `t_chain_data`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `from` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '发送人地址',
+  `to` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '接收人地址',
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '上链数据',
+  `hash_no` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '交易哈希值',
+  `block_index` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '位于哪一个区块',
+  `create_time` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '创建时间',
+  `code` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '溯源码',
+  `product_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '产品名',
+  `process_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '流程名',
+  `chain_status` varchar(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '0未上链，1上链成功',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '上链表' ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of t_chain_data
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for t_code
+-- ----------------------------
+DROP TABLE IF EXISTS `t_code`;
+CREATE TABLE `t_code`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `code` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '交易码',
+  `create_time` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '创建时间',
+  `product_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '产品名',
+  `address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '钱包地址',
+  `img_url` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '二维码链接',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of t_code
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for t_product
+-- ----------------------------
+DROP TABLE IF EXISTS `t_product`;
+CREATE TABLE `t_product`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '产品名',
+  `product_desc` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '产品描述',
+  `create_time` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '创建时间',
+  `address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '地址',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '产品表' ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of t_product
+-- ----------------------------
+
+SET FOREIGN_KEY_CHECKS = 1;
